8ddd89d30f8c32eb4a2c5d8f56af658e293c0c9d
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-01 12:46:16 +0300

    initial commit

---
+++ js/data/course-instructor-data.js
@@ -0,0 +1,27 @@
+const instructorData = [
+    {
+        name: `Sarah Taylor`,
+        occupation: `UX Consultant & Expertise`,
+        achievement1: `Top Instructor in Skillshare`,
+        achievement2: `30K+ Students`,
+        achievement3: `10 total courses`,
+        About: `The course has been really helpful.
+         I have been able to understand the fundamentals ofUI Design.
+         The importance of wireframing, prototyping, colours and how to
+         study other site wireframing and to implement them into
+         my UI/UX Creative process.`,
+    },
+    {
+        name: `Ran Segall`,
+        occupation: `UX Industry Expert`,
+        achievement1: `Top Instructor in Skillshare`,
+        achievement2: `30K+ Students`,
+        achievement3: `10 total courses`,
+        About: `The course has been really helpful.
+         I have been able to understand the fundamentals ofUI Design.
+         The importance of wireframing, prototyping, colours and how to
+         study other site wireframing and to implement them into
+         my UI/UX Creative process.`,
+    },
+];
+export {instructorData};
