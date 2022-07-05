a63fb1622be18cf87035d5dbd73e6a5773995d48
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-05 15:05:05 +0300

    started js for lightbox

---
+++ js/components/lightbox.js
@@ -0,0 +1,17 @@
+/*Lightbox start*/
+
+let box = document.getElementById('lightBox');
+
+let play = document.getElementById('playBtn');
+
+let close = document.getElementById('closeBtn');
+
+play.onclick = function(){
+    lightbox.style.display = 'block';
+}
+
+
+
+
+//  playBtn closeBtn lightBox
+/*Lightbox end*/
