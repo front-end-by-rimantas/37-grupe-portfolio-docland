36d3e5517efbc13ccc198b82a503d16f10209734
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-12 15:06:23 +0300

    js carousel

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,10 +1,36 @@
 const cards = document.querySelector('.imgBox');
-const slides = Array.from(cards.children)
+const slides = Array.from(cards.children);
 
 const leftBtn = document.querySelector('#slide-left');
 const rightBtn = document.querySelector('#slide-right');
 
-console.log(leftBtn);
+const slideWidth = slides[0].getBoundingClientRect().width;
+console.log(slideWidth);
 
+const moveSlidePostion = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px'
+};
 
+slides.forEach(moveSlidePostion);
+
+const moveToSlide = (cards, currentSlide, targetSlide) => {
+    cards.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+    
+};
+
+leftBtn.addEventListener('click', () => {
+    const currentSlide = cards.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(cards, currentSlide, targetSlide);
+});
+
+rightBtn.addEventListener('click', () => {
+    const currentSlide = cards.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(cards, currentSlide, targetSlide);
+});
         
