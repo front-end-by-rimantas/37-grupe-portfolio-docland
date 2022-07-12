2c80ff9f0978d01039c0726929964010dfffa8d3
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-12 09:48:20 +0300

    carousel.js

---
+++ js/components/carousel.js
@@ -0,0 +1,10 @@
+const cards = document.querySelector('.imgBox');
+const slides = Array.from(cards.children)
+
+const leftBtn = document.querySelector('#slide-left');
+const rightBtn = document.querySelector('#slide-right');
+
+console.log(leftBtn);
+
+
+        
